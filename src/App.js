8b855ab8d8ff6d727c024517ebd016005a49fdd2
toletole412 <yoonjioh90@gmail.com>
2018-03-11 00:33:30 +0100

    add createButton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Board from './containers/Board'
+import CreateGameButton from './components/CreateGameButton'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="title">0hh1</h1>
+        <div className="actions">
+          <CreateGameButton />
+        </div>
+
         <Board />
       </div>
     );
