97c3ee415a67e0c1ddee837683d568fa66bf848c
toletole412 <yoonjioh90@gmail.com>
2018-03-11 00:34:42 +0100

    add locked reducer

--- src/reducers/locked.js
+++ src/reducers/locked.js
@@ -1,3 +1,12 @@
-export default function( state = [], { type, payload } = {}) {
-  return state
+
+import { CREATE_GAME } from '../actions/types'
+
+export default (state = [], { type, payload } = {}) => {
+  switch (type) {
+    case CREATE_GAME :
+      return [].concat(payload.locked)
+
+    default:
+      return state
+  }
 }
