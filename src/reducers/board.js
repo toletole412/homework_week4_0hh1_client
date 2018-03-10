32bf633e89515c042bd7f7e32d6b738ed62f23ca
toletole412 <yoonjioh90@gmail.com>
2018-03-11 00:34:19 +0100

    add MOVE action

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,3 +1,5 @@
+import { CREATE_GAME, MOVE } from '../actions/types'
+
 const initialState = [
   [0,0,0,0,0,0,],
   [0,0,0,0,0,0,],
@@ -8,5 +10,28 @@ const initialState = [
 ]
 
 export default function(state = initialState, { type, payload } = {}) {
-  return state
-}
+  switch (type) {
+    case CREATE_GAME :
+      return [].concat(payload.board)
+
+    case MOVE :
+        return state
+          .map((row, rowIndex) => {
+            if (rowIndex !== payload.row) return row
+
+            return row.map((value, colIndex) => {
+              if (colIndex !== payload.col) return value
+
+              switch (value) {
+                case 0: return 1
+                case 1: return 2
+                default: return 0
+              }
+            })
+          })
+
+
+      default :
+        return state
+    }
+  }
