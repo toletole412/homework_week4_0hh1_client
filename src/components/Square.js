1b95705d6f73c420aedc35b16cf1d23418a385b0
toletole412 <yoonjioh90@gmail.com>
2018-03-11 12:01:39 +0100

    fix open state

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,51 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { move } from '../actions/game'
 import './Square.css'
 
-class Square extends PureComponent {
+export class Square extends PureComponent {
   static propTypes = {
     value: PropTypes.number.isRequired,
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+    makeMove: PropTypes.func.isRequired,
+    locked: PropTypes.bool,
+    dupe: PropTypes.bool,
+    error: PropTypes.bool
+  }
+
+  handleClick = () => {
+    const { x, y, makeMove, locked } = this.props
+    if (locked) return
+
+    makeMove(y, x)
+  }
+
+  classNames() {
+    const { value, locked, dupe, error } = this.props
+
+    let classnames = ['Square']
+    classnames.push(`fill-${value || 0}`)
+    if (locked) classnames.push('locked')
+    if (dupe) classnames.push('dupe')
+    if (error) classnames.push('wrong')
+
+    return classnames.join(' ')
   }
 
   render() {
     return (
-      <div className="Square" />
+      <div
+        className={this.classNames()}
+        onClick={this.handleClick}
+      />
     )
   }
 }
 
-export default Square
+const mapStateToProps = ({ locked }, { x, y }) => ({
+  locked: locked.filter(l => l[0] === y && l[1] === x).length > 0
+})
+
+export default connect(mapStateToProps, { makeMove: move })(Square)
